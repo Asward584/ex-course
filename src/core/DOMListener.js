8fed41c9737a1660893fa820c94dfcac35179953
Mary <mari.stagnacheva@gmail.com>
2021-02-12 17:37:29 +0300

    Version at 12th feburary

---
+++ src/core/DOMListener.js
@@ -0,0 +1,10 @@
+export class DOMListener {
+    
+    constructor($root){
+        if (!$root){
+            throw new Error('No $root for DOMListener')
+        }
+        this.root = $root;
+    }
+    
+}
