abe791e7919d4e1dd6f8170190a084ac279bb363
Mary <mari.stagnacheva@gmail.com>
2021-02-13 16:25:05 +0300

    with DOMclasss

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,31 +1,22 @@
-export class Excel{
-    constructor(selector, options){
-        this.$el = document.querySelector(selector)
-        this.components = options.components || []
-    }
+import { $ } from '../../core/dom';
+export class Excel {
+  constructor(selector, options) {
+    this.$el = $(selector);
+    this.components = options.components || [];
+  }
 
-    getRoot(){
-    
-        const $root = document.createElement('div')
-        $root.classList.add('excel')
+  getRoot() {
+    const $root = $.create('div', 'excel');
+    this.components.forEach((Component) => {
+      const $el = $.create('div', Component.className);
+      const component = new Component($el);
+      $el.html(component.toHTML());
+      $root.append($el);
+    });
+    return $root;
+  }
 
-        this.components.forEach(Component=>{        
-            const $el = document.createElement('div')
-            $el.classList.add(Component.className)
-            const component = new Component($el)
-            $el.innerHTML = component.toHTML()
-            console.log(component)
-            $root.appendChild($el)
-            
-        })
-        return $root
-    }
-
-    render (){
-
-        
-        this.$el.append(this.getRoot())
-        
-        
-    }
+  render() {
+    this.$el.append(this.getRoot());
+  }
 }
